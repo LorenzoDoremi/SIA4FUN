8483876467e00e5b6b7473e4baa8f0ad696733cf
Lorenzo Doremi <73567993+LorenzoDoremi@users.noreply.github.com>
2024-01-26 17:29:38 +0100

    added GET, added bind params

--- singleUserPosts.php
+++ singleUserPosts.php
@@ -7,10 +7,10 @@ $utente = $_GET["utente"];
 $prezzoMax = 200;
 $prezzoMin = 0;
 // PREPARO LA MIA QUERY. QUESTA TECNICA PREVIENE L'HACKING BASE!
-$query = $connessione->prepare('SELECT post.contenuto,utente.nome,utente.id FROM post,utente WHERE utente.id=? AND post.id=utente.id');
+$query = $connessione->prepare('SELECT post.contenuto,utente.nome,utente.id FROM post,utente WHERE utente.id=? AND post.idutente=utente.id');
 // sostituisco i punti di domanda con la variabile q
 // dd è il tipo. d è double. s si usa per parole. i numeri interi. 
-// $query->bind_param("s", $utente);
+$query->bind_param("d", $utente);
 $query->execute();
 $risultato = $query->get_result();
 
@@ -25,9 +25,7 @@ while ($row = mysqli_fetch_assoc($risultato)) {
 echo "<table class=\"table table-dark table-striped table-bordered\">";
 // creo la prima riga giusto per mettere i nomi delle colonna
 echo "<tr>";
-    echo "<td> meme </td>";
-    echo "<td> nome</td>";
-    echo "<td> id </td>";
+    echo "<h1>".$utente."</h1>";
 echo "</tr>";
 
 
@@ -37,7 +35,7 @@ foreach ($dati as $row) {
     echo "<tr>";
         echo "<td>".$row["contenuto"]."</td>";
         echo "<td>".$row["nome"]."</td>";
-        echo "<td>".$row["id"]."</td>";
+        echo "<td>".$utente."</td>";
     echo "</tr>";
    
 }

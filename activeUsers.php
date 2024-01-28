f937d52d72c0c50dd47846d3e675a860c0430692
Lorenzo Doremi <73567993+LorenzoDoremi@users.noreply.github.com>
2024-01-28 10:11:43 +0100

    added list of active users/ added prototype UI
    added a php function to get the list of unique active users who have at least one post. 
    included a simple li UI to display them through getPosts.

---
+++ activeUsers.php
@@ -0,0 +1,15 @@
+<?php
+function addActiveUser(&$users, $row) {
+
+    $found = false;
+    // cerco tra gli utenti già inseriti se l'ho già visto. altrimenti lo inserisco. 
+    foreach($users as $us) {
+        if($us["id"] == $row["id"]) {
+            $found = true;
+        }
+    }
+    if(!$found) {
+        $users[] = $row;
+    }
+}
+

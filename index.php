3a8ee72fbe0ccfae80cb8a421b28baa8198edf82
alessandropic <152594050+alessandropic@users.noreply.github.com>
2024-02-10 10:50:06 +0100

    index.php

--- index.php
+++ index.php
@@ -1,56 +1,103 @@
-<html>
-    <head>
-        <title>Spotify</title>
-        <?php require("head.html")?>
-    <body>
-        <div id="stemma">
-            <img src="static/Spotify.png" alt="">
+<style>
+      .post{
+        position: relative;
+        max-width: 100%;
+        width: 400px;
+     
+        border: 2px solid black;
+        border-radius: 5px 5px 5px 5px;
+        margin: 5px; 
+    }
+    .box p{
+        display: inline-block;
+        border: 2px solid red;
+    }
+    .post-list {
+        padding-top: 100px;
+    }
+    #add{
+        padding-top: 100px;
+        position: absolute;
+        right: 0;
+        color: white;
+    }
+    button{
+        background-color:rgb(70,0,218);
+        color: white;
+        border: 2px solid white;
+        border-radius: 12px !important; 
+        padding-right:10px;
+    }
+    button:hover{
+        background-color:white;
+        color: rgb(70,0,218);
+        border: 2px solid rgb(70,0,218);
+        border-radius: 12px;
+    }
+</style>
+<!doctype html>
+<html id="html" lang="en">
+  <head>
+  
+    <title>Timerr</title>
+    <?php require("head.html"); ?>
+
+</head>
+  <body>
+
+  <div class="container-fluid"> 
+    <?php require("navigazione.html"); ?>   
+    <div class="container-fluid" id="content">
+    </div>
+      <div id="add">
+            <button type="button"><a href="aggiungipost.php">AGGIUNGI POST</a></button>
         </div>
-        <?php require("navigazione.html") ?>
-        <p id="top">La tua Top 4 artisti</p>
-        <div class="card" >
-            <img src="static/nayt.jpg" alt="">
-            <div class="descrizione  nayt" >
-                <h2><a href="paginanayt.php">Nayt</a></h2>
-            </div>
-            <div class="box nayt ">
-                    <p>729.025 ascoltatori mensili</p>
-                    <p>9 novembre 1994</p>
-                    <p>Genere: Hip-hop/Rap</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="static/sfera.jpg" alt="">
-            <div class="descrizione sfera" >
-                <h2><a href="paginasfera.php">Sfera Ebbasta</a></h2>
-            </div>
-            <div class="box sfera ">
-                    <p>8,2 Mln ascoltatori mensili</p>
-                    <p>7 dicembre 1992</p>
-                    <p>Genere: Hip-hop/Rap</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="static/paky.jpg" alt="">
-            <div class="descrizione paky" >
-                <h2><a href="paginapaky.php">Paky</a></h2>
-                </div>
-            <div class="box paky">
-                    <p>2,4 Mln ascoltatori mensili</p>
-                    <p>28 novembre 1999</p>
-                    <p>Genere: Hip hop; Trap; Gangsta rap; Drill</p>
-            </div>
-        </div>
-        <div class="card">
-            <img src="static/ince.jpg" alt="">
-            <div class="descrizione ince" >
-                <h2><a href="paginaince.php">Ince</a></h2>
-                </div>
-            <div class="box ince">
-                    <p>71.983 ascoltatori mensili</p>
-                    <p>26 ottobre 2000</p>
-                    <p>Genere:Trap; Gangsta rap; Drill</p>
-            </div>
-        </div>
-    </body>
+      <?php
+          require("connessione.php");
+
+          $prezzoMax = 200;
+          $prezzoMin = 0;
+          // PREPARO LA MIA QUERY. QUESTA TECNICA PREVIENE L'HACKING BASE!
+          $query = $connessione->prepare('SELECT post.contenuto,utente.nome,utente.id FROM post,utente WHERE post.idutente=utente.id');
+          // sostituisco i punti di domanda con la variabile q
+          // dd è il tipo. d è double. s si usa per parole. i numeri interi. 
+          // $query->bind_param("dd", $prezzoMin, $prezzoMax);
+          $query->execute();
+          $risultato = $query->get_result();
+
+          // memorizzo i miei dati per poterli riutilizzare in futuro
+          $dati = array();
+          while ($row = mysqli_fetch_assoc($risultato)) {
+              $dati[]= $row;
+          }
+
+
+          echo "<div class=\"post-list\">";
+          // inersico i dati della query che ho fatto al database. 
+          
+          foreach ($dati as $row) {
+
+            echo "<div class=\"post\"> ";
+            echo      "<img src=\"static/\" alt=\"\"> </img>";
+            echo      "<p>".$row["nome"]."</p>";
+            echo      "<div class=\"descrizione\">";
+            echo           "<p>".$row["contenuto"]."</p>";
+            echo      "</div>";
+            echo      "<div class=\"box\">";
+            echo           "<p>like</p> <p>commenti</p> ";
+            echo      "</div>";
+            echo "</div>";
+          }   
+          
+          
+      
+          
+        ?>
+      
+   
+
+</div>
+
+</body>
+<script src="static/bootstrap.bundle.min.js"></script>
 </html>
